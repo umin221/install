18d77087b3bdbe898cf2219c9f743c26766ccace
邓友明 <dengyouming@kingnode.com>
2018-01-26 19:34:04 +0800

    common

---
+++ src/wechat/demo/page/loading.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <mt-header fixed title="demo">
+      <fallback slot="left">返回</fallback>
+    </mt-header>
+
+    <list-loading></list-loading>
+
+  </div>
+</template>
+
+<script type="application/javascript">
+  import listLoading from 'public/components/list-loading';
+
+  export default {
+    name: 'list-loading',
+    components: listLoading
+  };
+</script>
