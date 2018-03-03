1cd059f25b94c8036e3125fb212071bd3ffaace8
qjm <qinjm@coracle.com>
2018-03-03 17:01:08 +0800

    添加跳转

--- src/wechat/workbench/page/index.vue
+++ src/wechat/workbench/page/index.vue
@@ -6,7 +6,7 @@
     <mt-cell title="标题文字" is-link>
       <span style="color: green">这里是元素</span>
     </mt-cell>
-    <mt-cell  v-for="(item, index) in listObj" :key="index" :title="item.vul"  @click.native="getList" is-link>
+    <mt-cell  v-for="(item, index) in listObj" :key="index" :title="item.vul"  @click.native="getUrl(item.id)" is-link>
 <!--
       <img slot="icon" src="" width="24" height="24">
 -->
@@ -16,7 +16,7 @@
 
 <script type="application/javascript">
   // import api from '../api/api';
-  import {mapState, mapActions} from 'vuex';
+  import {mapState} from 'vuex';
 
   const NameSpace = 'index';
   export default {
@@ -33,16 +33,16 @@
       return {
         active: 'tab-container1',
         listObj: [
-          {'id': 1, 'vul': '工作计划'},
+          {'id': 'workPlan', 'vul': '工作计划'},
           /* {'id': 1, 'vul': '安装打卡'},
           {'id': 1, 'vul': '安装交接单'},*/
-          {'id': 1, 'vul': '安装订单'},
-          {'id': 1, 'vul': '维修工单'},
+          {'id': 'installOrder', 'vul': '安装订单'},
+          {'id': 'serviceOrder', 'vul': '维修工单'},
           /* {'id': 1, 'vul': '维修订单'},
           {'id': 1, 'vul': '我的配件'},
           {'id': 1, 'vul': '配件领用'},
           {'id': 1, 'vul': '配件退入'},*/
-          {'id': 1, 'vul': '委外人员管理'}
+          {'id': 'outsourcing', 'vul': '委外人员管理'}
         ]
       };
     },
@@ -50,7 +50,9 @@
       ...mapState(NameSpace, ['value'])
     },
     methods: {
-      ...mapActions(NameSpace, ['getList'])
+      getUrl(id) {
+        document.location = '../' + id + '/index.html';
+      }
     }
   };
 </script>
