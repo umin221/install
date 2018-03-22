7e60c835299e04ce449cd8933ab4356d594b9af1
邓友明 <dengyouming@kingnode.com>
2018-03-22 21:34:04 +0800

    委外团队－附件补充

--- src/public/store/index.js
+++ src/public/store/index.js
@@ -24,7 +24,6 @@ export const app = {
     getLov({state}, option) {
       let data = option.data || {Type: option.type};
       ajax(Object.assign({
-        method: 'get',
         cache: true, // PickList Hierarchical
         url: 'data/List Of Values/List Of Values/?searchspec=Active="Y" AND Language="CHS" AND ' + KND.Util.condition(data) + '&PageSize=100&StartRowNum=0',
         success: data => {
@@ -38,16 +37,36 @@ export const app = {
      */
     getAuthority({state}, option) {
       ajax({
-        method: 'get',
         url: ('https://kas.kinlong.cn:8090/webchat/api/local/permission?url=' + encodeURIComponent(location.href.split('#')[0]) + '&appNO=CONTACT'),
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
         },
         success: data => {
+          data.debug = true;
           wx.config(data);
           wx.ready(() => {
             console.log('WeChatAPI success');
           });
+        }
+      });
+    },
+
+    /**
+     * 获取附件
+     */
+    getMediaById({state}, option) {
+      ajax({
+        method: 'post',
+        url: 'service/Workflow Process Manager/RunProcess',
+        data: {
+          'body': {
+            'ProcessName': 'KL Attachment Base64 Query Process',
+            'Type': option.type, // 'Partner',
+            'Object Id': option.id // '1-2BSEPVGX'
+          }
+        },
+        success: data => {
+          console.log(123);
         }
       });
     }
