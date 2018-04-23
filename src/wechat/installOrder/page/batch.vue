991e4d6b47612598cb6b98ad3542c897e981b42b
qjm <qinjm@coracle.com>
2018-04-23 19:55:42 +0800

    安装订单-详情-计划

--- src/wechat/installOrder/page/batch.vue
+++ src/wechat/installOrder/page/batch.vue
@@ -9,7 +9,7 @@
         <mt-cell title="批次">
           <span>{{batchCode}}</span>
         </mt-cell>
-       <!-- <cus-field label="计划开始日期" tag="计划开始日期"
+        <cus-field label="计划开始日期" tag="计划开始日期"
                    @click.native="open('picker')"
                    v-model="start_Date"
                    v-valid.require
@@ -18,7 +18,7 @@
                    @click.native="open('pickerEnd')"
                    v-model="end_Date"
                    v-valid.require
-                   is-link></cus-field>-->
+                   is-link></cus-field>
         <cus-field label="计划数量" tag="计划数量"
                    placeholder="请输入"
                    v-valid.require
