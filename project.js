09c48a196b7c79ff4d82284dd1c8a2bd4ca81777
邓友明 <dengyouming@kingnode.com>
2018-04-23 16:10:40 +0800

    root－清除配置

--- project.js
+++ project.js
@@ -1,6 +1,6 @@
 var project = {
-  authorization: 'HELLO', // 'HELLO' 'ACCOUNT'  // siebel 认证模式，除dev环境为 帐号:帐号 , 其他环境为 帐号:HELLO
-  host: 'https://kas.kinlong.cn:8090', // 服务器地址
-  context: '/crm/sit/api/', //
-  debug: false
+  //authorization: 'HELLO', // 'HELLO' 'ACCOUNT' => siebel 认证模式，除dev环境为 帐号:帐号 , 其他环境为 帐号:HELLO
+  //host: 'https://kas.kinlong.cn:8090', // 服务器地址
+  //context: '/crm/sit/api/', //
+  //debug: false
 };
