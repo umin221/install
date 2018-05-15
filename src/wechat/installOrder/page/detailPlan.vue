0484f1a700c48cd5a8ded60db74eee07c9f0850d
qjm <qinjm@coracle.com>
2018-05-15 15:24:37 +0800

    安装订单-详情-批次详细计划时间限制

--- src/wechat/installOrder/page/detailPlan.vue
+++ src/wechat/installOrder/page/detailPlan.vue
@@ -287,15 +287,15 @@
       submitFn() {
         var self = this;
         tools.valid.call(this, () => {
-          if (self.planObj.Planned > self.planObj['Planned Completion']) {
-            console.dir('计划开始日期不能大于计划完成日期');
-            Toast('计划开始日期不能大于计划完成日期');
+          if (self.planObj.Planned >= self.planObj['Planned Completion']) {
+            console.dir('计划开始日期不能大于等于计划完成日期');
+            Toast('计划开始日期不能大于等于计划完成日期');
             return;
           }
           if (self.planObj.Started) {
-            if (self.planObj.Started > self.planObj.Done) {
-              console.dir('实际开始日期不能大于实际结束日期');
-              Toast('实际开始日期不能大于实际结束日期');
+            if (self.planObj.Started >= self.planObj.Done) {
+              console.dir('实际开始日期不能大于等于实际结束日期');
+              Toast('实际开始日期不能大于等于实际结束日期');
               return;
             }
           }
