17a5dcd9b85554cbeb8740a2cbca0679d295f853
邓友明 <dengyouming@kingnode.com>
2018-05-22 20:51:26 +0800

    工作台-uat环境用户配置

--- src/wechat/workbench/page/switch.vue
+++ src/wechat/workbench/page/switch.vue
@@ -18,80 +18,15 @@
 </template>
 
 <script type="es6">
+  import list from '../store/users';
+  // 环境
+  let env = global['project']['env'] || {env: 'SIT'};
   let NameSpace = 'default';
   export default {
     name: NameSpace,
     data() {
       return {
-        list: [{
-          userID: '16043202', position: '安装主管', area: '华东安装四组', name: '张世虎'
-        }, {
-          userID: '18020016', position: '产品安装工程师', area: '华东安装四组', name: '刘家军'
-        }, {
-          userID: '17023092', position: '产品安装工程师', area: '华东安装四组', name: '刘耀'
-        }, {
-          userID: '16043218', position: '安装主管', area: '北方安装一组', name: '谢应发'
-        }, {
-          userID: '17023093', position: '产品安装工程师', area: '北方安装一组', name: '贾思杰'
-        }, {
-          userID: '16093008', position: '产品安装工程师', area: '北方安装一组', name: '王照勇'
-        }, {
-          userID: '16033164', position: '安装主管', area: '南方安装一组', name: '叶亮'
-        }, {
-          userID: '16033168', position: '产品安装工程师', area: '南方安装一组', name: '曹庆红'
-        }, {
-          userID: '17023097', position: '产品安装工程师', area: '南方安装一组', name: '丁圣'
-        }, {
-          userID: '16043215', position: '门厂技术主管（代）', area: '门厂技术组', name: '胡清秀'
-        }, {
-          userID: '17053018', position: '门厂技术工程师', area: '门厂技术组', name: '韦吉辉'
-        }, {
-          userID: '17080257', position: '门厂技术工程师', area: '门厂技术组', name: '覃仕权'
-        }, {
-          userID: '1651', position: '部长（代）', area: '', name: '郭利勤'
-        }, {
-          userID: '14083015', position: '部长（代）', area: '', name: '胡成涛'
-        }, {
-          userID: '10080387', position: '部长（代）', area: '', name: '林陈根'
-        }, {
-          userID: '13070453', position: '部长（代）', area: '', name: '周憧'
-        }, {
-          userID: '16113009', position: '客服主管', area: '', name: '袁静'
-        }, {
-          userID: '13030172', position: '安装支持主管（代）', area: '产品安装一组（华东）', name: '张长虹'
-        }, {
-          userID: '18010047', position: '安装支持专员', area: '产品安装一组（华东）', name: '周振威'
-        }, {
-          userID: '18010174', position: '安装支持专员', area: '产品安装一组（华东）', name: '刘振伟'
-        }, {
-          userID: '15063016', position: '安装支持主管（代）', area: '产品安装二组（北方）', name: '李志勇'
-        }, {
-          userID: '14030364', position: '安装支持专员', area: '产品安装二组（北方）', name: '荣誉'
-        }, {
-          userID: '17070706', position: '安装支持专员', area: '产品安装二组（北方）', name: '魏春婷'
-        }, {
-          userID: '11040118', position: '安装支持主管（代）', area: '产品安装三组（南方）', name: '刘继莲'
-        }, {
-          userID: '17043013', position: '安装支持专员', area: '产品安装三组（南方）', name: '王翠仪'
-        }, {
-          userID: '17023089', position: '安装支持专员', area: '产品安装三组（南方）', name: '刘辉'
-        }, {
-          userID: '12070429', position: '项目支持主管（代）', area: '产品安装支持组', name: '周红娟'
-        }, {
-          userID: '18010049', position: '项目支持专员', area: '产品安装支持组', name: '周理永'
-        }, {
-          userID: '17070565', position: '技术支持专员', area: '技术支持组', name: '全俊龙'
-        }, {
-          userID: '14080088', position: '客服专员', area: '产品安装支持组', name: '张沙沙'
-        }, {
-          userID: '17080258', position: '客服专员', area: '产品安装支持组', name: '许壁填'
-        }, {
-          userID: 'IM01', position: '产品安装主管', area: '00000010', name: 'IM01'
-        }, {
-          userID: 'IE01', position: '产品安装工程师', area: '00000001', name: 'IE01'
-        }, {
-          userID: 'DE01', position: '门厂技术工程师', area: '00000040', name: 'DE01'
-        }]
+        list: list[env]
       };
     },
     methods: {
---
+++ src/wechat/workbench/store/users.js
@@ -0,0 +1,169 @@
+/**
+ * sit 测试用户清单
+ * @type {*[]}
+ */
+let SIT = [{
+  userID: '16043202', position: '安装主管', area: '华东安装四组', name: '张世虎'
+}, {
+  userID: '18020016', position: '产品安装工程师', area: '华东安装四组', name: '刘家军'
+}, {
+  userID: '17023092', position: '产品安装工程师', area: '华东安装四组', name: '刘耀'
+}, {
+  userID: '16043218', position: '安装主管', area: '北方安装一组', name: '谢应发'
+}, {
+  userID: '17023093', position: '产品安装工程师', area: '北方安装一组', name: '贾思杰'
+}, {
+  userID: '16093008', position: '产品安装工程师', area: '北方安装一组', name: '王照勇'
+}, {
+  userID: '16033164', position: '安装主管', area: '南方安装一组', name: '叶亮'
+}, {
+  userID: '16033168', position: '产品安装工程师', area: '南方安装一组', name: '曹庆红'
+}, {
+  userID: '17023097', position: '产品安装工程师', area: '南方安装一组', name: '丁圣'
+}, {
+  userID: '16043215', position: '门厂技术主管（代）', area: '门厂技术组', name: '胡清秀'
+}, {
+  userID: '17053018', position: '门厂技术工程师', area: '门厂技术组', name: '韦吉辉'
+}, {
+  userID: '17080257', position: '门厂技术工程师', area: '门厂技术组', name: '覃仕权'
+}, {
+  userID: '1651', position: '部长（代）', area: '', name: '郭利勤'
+}, {
+  userID: '14083015', position: '部长（代）', area: '', name: '胡成涛'
+}, {
+  userID: '10080387', position: '部长（代）', area: '', name: '林陈根'
+}, {
+  userID: '13070453', position: '部长（代）', area: '', name: '周憧'
+}, {
+  userID: '16113009', position: '客服主管', area: '', name: '袁静'
+}, {
+  userID: '13030172', position: '安装支持主管（代）', area: '产品安装一组（华东）', name: '张长虹'
+}, {
+  userID: '18010047', position: '安装支持专员', area: '产品安装一组（华东）', name: '周振威'
+}, {
+  userID: '18010174', position: '安装支持专员', area: '产品安装一组（华东）', name: '刘振伟'
+}, {
+  userID: '15063016', position: '安装支持主管（代）', area: '产品安装二组（北方）', name: '李志勇'
+}, {
+  userID: '14030364', position: '安装支持专员', area: '产品安装二组（北方）', name: '荣誉'
+}, {
+  userID: '17070706', position: '安装支持专员', area: '产品安装二组（北方）', name: '魏春婷'
+}, {
+  userID: '11040118', position: '安装支持主管（代）', area: '产品安装三组（南方）', name: '刘继莲'
+}, {
+  userID: '17043013', position: '安装支持专员', area: '产品安装三组（南方）', name: '王翠仪'
+}, {
+  userID: '17023089', position: '安装支持专员', area: '产品安装三组（南方）', name: '刘辉'
+}, {
+  userID: '12070429', position: '项目支持主管（代）', area: '产品安装支持组', name: '周红娟'
+}, {
+  userID: '18010049', position: '项目支持专员', area: '产品安装支持组', name: '周理永'
+}, {
+  userID: '17070565', position: '技术支持专员', area: '技术支持组', name: '全俊龙'
+}, {
+  userID: '14080088', position: '客服专员', area: '产品安装支持组', name: '张沙沙'
+}, {
+  userID: '17080258', position: '客服专员', area: '产品安装支持组', name: '许壁填'
+}, {
+  userID: 'IM01', position: '产品安装主管', area: '00000010', name: 'IM01'
+}, {
+  userID: 'IE01', position: '产品安装工程师', area: '00000001', name: 'IE01'
+}, {
+  userID: 'DE01', position: '门厂技术工程师', area: '00000040', name: 'DE01'
+}];
+
+/**
+ * uat 测试用户清单
+ * @type {*[]}
+ */
+let UAT = [{
+  userID: '14083015', position: '副部长（代）', area: '安装支持一部', name: '胡成涛'
+}, {
+  userID: '13030172', position: '安装支持主管', area: '华东安装支持组', name: '张长虹'
+}, {
+  userID: '18010047', position: '安装支持专员', area: '华东安装支持组', name: '周振威'
+}, {
+  userID: '18010174', position: '安装支持专员', area: '华东安装支持组', name: '刘振伟'
+}, {
+  userID: '18030946', position: '安装支持专员', area: '华东安装支持组', name: '周伟'
+}, {
+  userID: '17030374', position: '安装支持专员', area: '华东安装支持组', name: '李舒棕'
+}, {
+  userID: '10080387', position: '副部长（代）', area: '安装支持二部', name: '林陈根'
+}, {
+  userID: '15063016', position: '安装支持主管', area: '北方安装支持组', name: '李志勇'
+}, {
+  userID: '14030364', position: '安装支持专员', area: '北方安装支持组', name: '荣誉'
+}, {
+  userID: '17070706', position: '安装支持专员', area: '北方安装支持组', name: '魏春婷'
+}, {
+  userID: '17120643', position: '安装支持专员', area: '北方安装支持组', name: '赵颖婷'
+}, {
+  userID: '13070453', position: '副部长（代）', area: '安装支持三部', name: '周憧'
+}, {
+  userID: '11040118', position: '安装支持主管', area: '南方安装支持组', name: '刘继莲'
+}, {
+  userID: '17023083', position: '安装支持专员', area: '南方安装支持组', name: '黄连侨'
+}, {
+  userID: '17023089', position: '安装支持专员', area: '南方安装支持组', name: '刘辉'
+}, {
+  userID: '17043013', position: '安装支持专员', area: '南方安装支持组', name: '王翠仪'
+}, {
+  userID: '1651', position: '副部长（代）', area: '安装支持三部', name: '郭利勤'
+}, {
+  userID: '16113009', position: '客服主管', area: '售后客服组', name: '袁静'
+}, {
+  userID: '18031034', position: '客服专员', area: '售后客服组', name: '李锦凤'
+}, {
+  userID: '17080258', position: '客服专员', area: '售后客服组', name: '许壁填'
+}, {
+  userID: '14080088', position: '客服专员', area: '售后客服组', name: '张沙沙'
+}, {
+  userID: '12070429', position: '项目支持主管', area: '项目支持组', name: '周红娟'
+}, {
+  userID: '18040127', position: '项目支持专员', area: '项目支持组', name: '袁静'
+}, {
+  userID: '18010049', position: '项目支持专员', area: '项目支持组', name: '李锦凤'
+}, {
+  userID: '11020316', position: '技术支持主管', area: '技术支持组', name: '张文通'
+}, {
+  userID: '17070565', position: '技术支持专员', area: '技术支持组', name: '全俊龙'
+}, {
+  userID: '16043106', position: '安装主管（代）', area: '华东安装三组', name: '贺香龙'
+}, {
+  userID: '18010225', position: '产品安装工程师', area: '华东安装三组', name: '庞耀辉'
+}, {
+  userID: '16043202', position: '安装主管', area: '华东安装四组', name: '张世虎'
+}, {
+  userID: '16043210', position: '安装主管（代）', area: '华东安装四组', name: '王铁军'
+}, {
+  userID: '15120017', position: '产品安装工程师', area: '华东安装四组', name: '温明提'
+}, {
+  userID: '16033164', position: '安装主管', area: '南方安装一组', name: '叶亮'
+}, {
+  userID: '16043218', position: '安装主管', area: '北方安装一组', name: '谢应发'
+}, {
+  userID: '16093001', position: '安装主管', area: '北方安装四组', name: '李好好'
+}, {
+  userID: '18010190', position: '产品安装工程师', area: '北方安装四组', name: '郭飞红'
+}, {
+  userID: '16043215', position: '门厂技术组', area: '门厂技术主管（代）', name: '胡清秀'
+}, {
+  userID: '12090020', position: 'CRM应用组', area: '主管（代）', name: '周利尔'
+}, {
+  userID: '14123015', position: 'CRM应用组', area: '应用工程师', name: '薛哲'
+}, {
+  userID: '17063108', position: 'CRM应用组', area: '应用工程师', name: '张赫'
+}, {
+  userID: '17063032', position: 'CRM应用组', area: '应用工程师', name: '辛志伟'
+}, {
+  userID: '18031439', position: 'CRM应用组', area: '应用工程师', name: '卓玲'
+}, {
+  userID: '13030901', position: '销售应用部', area: '部长', name: '何毅'
+}, {
+  userID: '16123001', position: '信息管理中心', area: '总监', name: '袁红军'
+}, {
+  userID: '16023067', position: '订单管理中心', area: '副总监', name: '郑豫江'
+}];
+
+export default {DEV: SIT, SIT, UAT};
