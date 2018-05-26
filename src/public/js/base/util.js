df724f1081a674673616ddd8d86727515501ea8e
邓友明 <dengyouming@kingnode.com>
2018-05-26 14:41:05 +0800

    public-异常日期返回

--- src/public/js/base/util.js
+++ src/public/js/base/util.js
@@ -337,26 +337,30 @@
    */
   // eslint-disable-next-line
   Date.prototype.format = function(format) {
-    let o = {
-      'M+': this.getMonth() + 1, // month
-      'd+': this.getDate(), // day
-      'h+': this.getHours(), // hour
-      'm+': this.getMinutes(), // minute
-      's+': this.getSeconds(), // second
-      'q+': Math.floor((this.getMonth() + 3) / 3), // quarter
-      'S': this.getMilliseconds() // millisecond
-    };
+    if (this.getMonth() > -1) {
+      let o = {
+        'M+': this.getMonth() + 1, // month
+        'd+': this.getDate(), // day
+        'h+': this.getHours(), // hour
+        'm+': this.getMinutes(), // minute
+        's+': this.getSeconds(), // second
+        'q+': Math.floor((this.getMonth() + 3) / 3), // quarter
+        'S': this.getMilliseconds() // millisecond
+      };
 
-    if (/(y+)/.test(format)) {
-      format = format.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
-    }
-
-    for (let k in o) {
-      if (new RegExp('(' + k + ')').test(format)) {
-        format = format.replace(RegExp.$1, RegExp.$1.length === 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length));
+      if (/(y+)/.test(format)) {
+        format = format.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
       }
-    };
-    return format === 'NaN-aN-aN' ? '' : format;
+
+      for (let k in o) {
+        if (new RegExp('(' + k + ')').test(format)) {
+          format = format.replace(RegExp.$1, RegExp.$1.length === 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length));
+        }
+      };
+    } else {
+      format = '';
+    }
+    return format;
   };
 
   /**
