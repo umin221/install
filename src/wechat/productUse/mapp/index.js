7acd8c5968fdd09a74eab5c5c6a2c694a8900289
qjm <qinjm@coracle.com>
2018-06-06 20:47:43 +0800

    配件-wms驳回数据显示

--- src/wechat/productUse/mapp/index.js
+++ src/wechat/productUse/mapp/index.js
@@ -3,7 +3,7 @@
  */
 config.mapp = {
   'manager': {
-    '待处理': {status: 'Draft,Rejected', list: 'pending'},
+    '待处理': {status: 'Draft,Rejected,WMS Rejected', list: 'pending'},
     '处理中': {status: 'Awaiting Approval,Submitted', list: 'valid'},
     '已完成': {status: 'Closed', list: 'invalid'}
   }
