9b44d91fb7cb501990494bf300c80b9be49166f6
邓友明 <dengyouming@kingnode.com>
2018-06-22 21:09:25 +0800

    离线app-版本号调整

--- src/wechat/install/config.js
+++ src/wechat/install/config.js
@@ -4,11 +4,11 @@ Object.assign(config, {
   offline: true,
   android: {
 //  最新app版本号
-    version: '0.5.1'
+    version: '1.0.0'
   },
   ios: {
 //  最新app版本号
-    version: '0.5.1'
+    version: '1.0.0'
   }
 //  default 提示 / silent 静默
 //  upgrade: 'default'
